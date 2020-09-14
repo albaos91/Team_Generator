ccd928c832c5aed950483ddc2f6535c5b2389529
Alba Osmanaj <albaos91@gmail.com>
2020-09-13 20:09:36 -0500

    manager profiles created

---
+++ templates/ManagerProfile.js
@@ -0,0 +1,11 @@
+const EmployeeProfile = require("./EmployeeProfile");
+
+class ManagerProfile extends EmployeeProfile {
+  constructor(manager) {
+    super(manager);
+    this.officeNumber = manager.officeNumber;
+    this.setProfile();
+  }
+}
+
+module.exports = ManagerProfile;
