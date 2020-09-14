366b681d37b0e856701bdd2eda883e90caa37b4c
Alba Osmanaj <albaos91@gmail.com>
2020-09-13 20:08:51 -0500

    interns created

---
+++ templates/InternProfile.js
@@ -0,0 +1,11 @@
+const EmployeeProfile = require("./EmployeeProfile");
+
+class InternProfile extends EmployeeProfile {
+  constructor(intern) {
+    super(intern);
+    this.school = intern.school;
+    this.setProfile();
+  }
+}
+
+module.exports = InternProfile;
