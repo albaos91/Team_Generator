036ce8e1e5894b4aaf14f6bbb97a1c14f730649d
Alba Osmanaj <albaos91@gmail.com>
2020-09-13 20:08:14 -0500

    engineer profile templates created

---
+++ templates/EngineerProfile.js
@@ -0,0 +1,11 @@
+const EmployeeProfile = require("./EmployeeProfile");
+
+class EngineerProfile extends EmployeeProfile {
+  constructor(engineer) {
+    super(engineer);
+    this.github = engineer.github;
+    this.setProfile();
+  }
+}
+
+module.exports = EngineerProfile;
